8e805e1b64b7bfdc06dd016670dda24875a10696
SubwayMan <tyche4490@gmail.com>
2019-11-12 17:36:44 -0800

    fixed privacy issue: deleted local data

--- env_variables.py
+++
@@ -1,8 +0,0 @@
-#Confidential data: IF YOU ARE NOT TYLER, YOU SHOULDN'T BE HERE!
-botURL = 'NjQwNDM2NDEzNzgzMDgwOTcw.Xb54fw.g8P-8WPuiVnOzHycn2lJ1CY_cio'
-Database_log = 'mongodb+srv://Subway_Man:println40404$@mybotcluster-afbum.mongodb.net/test?retryWrites=true&w=majority'
-
-ServerId = 631998952161542164
-
-swears = ['fuck', 'shit', 'bitch', 'nigger', 'nigga', 'faggot']
-admin = 'SubwayMan#9950'
